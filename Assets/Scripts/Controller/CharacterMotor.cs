7ed081b78c56652405d9cb74d60066f890f77438
LunarHackett003 <LunarHackett003@users.noreply.github.com>
2024-02-05 19:53:12 +0000

    bendy man can aim

--- Assets/Scripts/Controller/CharacterMotor.cs
+++ Assets/Scripts/Controller/CharacterMotor.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Starlight
 {
     public class CharacterMotor : MonoBehaviour, IManagedBehaviour
     {
         [SerializeField] Transform aimRotationTransform;
+        [SerializeField] Transform aimRotationAnchor;
 
+        [SerializeField, Header("Aiming")] Vector2 lookInput;
+        [SerializeField] Vector2 lookSpeed;
+        [SerializeField] Vector2 lookAngle;
+        [SerializeField] float currentLeanAngle;
+        [SerializeField] float leanBounds, leanSpeed;
+        [SerializeField] float lookPitchClamp = 75;
 
-
+        [SerializeField, Space(10), Header("Movement")] Vector2 moveInput;
+        [SerializeField] Vector2 dampedMovement;
+        Vector2 dampVelocity;
+        [SerializeField] float movementRampSpeed, moveSpeedMultiplier;
         private void Awake()
         {
             BehaviourManager.fixedUpdateClients += ManagedFixedUpdate;
@@ -19,17 +30,33 @@ namespace Starlight
 
         public void ManagedFixedUpdate()
         {
-
+            dampedMovement = Vector2.SmoothDamp(dampedMovement, moveInput, ref dampVelocity, movementRampSpeed);
+            transform.Translate(moveSpeedMultiplier * Time.fixedDeltaTime * new Vector3(dampedMovement.x, 0, dampedMovement.y), Space.Self);
         }
 
         public void ManagedLateUpdate()
         {
-
+            aimRotationTransform.position = aimRotationAnchor.position;
+            lookAngle += lookInput * lookSpeed * Time.fixedDeltaTime;
+            lookAngle.y = Mathf.Clamp(lookAngle.y, -lookPitchClamp, lookPitchClamp);
+            aimRotationTransform.localRotation = Quaternion.Euler(lookAngle.y, 0, 0);
+            aimRotationTransform.localRotation *= Quaternion.Euler(0, 0, currentLeanAngle);
+            transform.localRotation = Quaternion.Euler(0, lookAngle.x, 0);
+            lookAngle.x %= 360;
         }
 
         public void ManagedUpdate()
         {
 
+        }
+        
+        public void GetMoveInput(InputAction.CallbackContext context)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+        public void GetLookInput(InputAction.CallbackContext context)
+        {
+            lookInput = context.ReadValue<Vector2>();
         }
     }
 }
